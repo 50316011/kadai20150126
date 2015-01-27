47d3ead285a5098257c0e31f40e069019f3f035a
50316011 <s0728_king@yahoo.co.jp>
2015-01-28 00:19:25 +0900

    20150126 submit

---
+++ src/Graph_main/Graph_main.java
@@ -0,0 +1,15 @@
+package Graph_main;
+
+public class Graph_main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Graph_View graph = new Graph_View();
+		graph.setBounds(5,5,655,455);
+		graph.setVisible(true);
+
+
+	}
+
+}
